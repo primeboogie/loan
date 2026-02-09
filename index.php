4852de6e2424ce20979d12213eed2e6d21c5f257
primeboogie <amososwom@gmail.com>
2026-02-09 21:31:24 +0300

    ss

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Admin-Key, x-admin-key");
 header("Access-Control-Max-Age: 86400");
+header("Access-Control-Allow-Credentials: true"); 
 
 // Handle preflight OPTIONS request
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
